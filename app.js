8adff30bc9addb3b540b1ddc2f350f837c98e06c
Ajay Rathod <rathodajay60@gmail.com>
2019-11-14 22:20:05 +0530

    CRUD added

--- app.js
+++ app.js
@@ -3,15 +3,38 @@ var mongoose =  require('mongoose')
 var bodyparser =  require('body-parser')
 var cors =  require('cors')
 var path =  require('path')
+var route =  require('./routes/route')
+
 
 var app = express();
 
-port= 3000
 
-url = 'http://localhost:'+port
+mongoose.connect('mongodb://localhost:27017/Contactlist');
+
+mongoose.connection.on('connected', ()=>{
+    console.log('Connected to mongodb @ 27017')
+})
+
+mongoose.connection.on('error', (err)=>{
+    
+    if(err) {
+        console.log('Error in the database connection:'+err)        
+    }
+})
+
+
+port= 3000;
+
+app.use('/api', route);
+
+app.use(bodyparser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+url = 'http://localhost:'+port;
 
 app.get('/', (req, res)=>{
-    res.send('Welcome to MEAN Stack!');
+    res.send('Welcome to MEAN Stack1!');
 })
 
 app.listen(port, ()=>{
